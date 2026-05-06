9a67a59c6400bdfd9cbfc3179aaa66b445d19eb4
Vimal Raj <vimalraj87464@gmail.com>
2026-05-06 15:49:01 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hospital Billing API Running');
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
